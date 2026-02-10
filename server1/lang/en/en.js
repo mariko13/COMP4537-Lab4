ac97738d89de1d4d2fbe6f6bb2c472ffecded6d6
MARIKO HOCKERTZ <mariko.hockertz@gmail.com>
2026-02-09 17:28:30 -0800

    Create STRINGS

---
+++ server1/lang/en/en.js
@@ -0,0 +1,18 @@
+const STRINGS = {
+    TITLE: "Patient Database",
+    
+    BUTTON_INSERT: "Insert Patient Data",
+
+    HEADER_GET_DATA: "Get Patient Data",
+    LABEL_QUERY: "Enter SQL query: ",
+    PLACEHOLDER_QUERY: "SELECT * FROM patient",
+    BUTTON_QUERY: "Submit",
+
+    HEADER_RESULT: "Result",
+    DEFAULT_OUTPUT: "Submit a query above to get results",
+    
+    INSERT_REQUEST: "Sending insert request...",
+    INSERT_ERROR: "Insert error: ",
+    RUNNING_QUERY: "Running query...",
+    QUERY_ERROR: "Query error: "
+}
